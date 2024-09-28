243b7b926e688d3a634526f3b11b0537580c78aa
Talha Ibn Aziz <talhaibnaziz6343@gmail.com>
2024-09-28 10:01:55 -0700

    First Commit

---
+++ systemInstall.sh
@@ -0,0 +1,8 @@
+### Necessary installations
+sudo apt update && sudo apt upgrade -y
+sudo apt install thunderbird
+sudo apt install libreoffice
+
+### Optional installations
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
